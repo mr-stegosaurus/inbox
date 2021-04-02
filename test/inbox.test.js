4bec0465d7f58973bfc1ab3e429bc9bd42af4ef8
Justin Smith <smithjustin158@gmail.com>
2021-04-02 00:04:03 -0400

    testing deployment

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -8,8 +8,24 @@ const ganache = require('ganache-cli');
 //This is ETH test network for contract deployment.
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile')
 //web3 allows for ethereum programming within the network
 
+let accounts;
+let inbox;
 
+beforeEach(async () =>{
+    //Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
 
-//test
+    // Use one of those accounts to deploy the contracts
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0], gas: '1000000' })
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(inbox);
+    });
+});
