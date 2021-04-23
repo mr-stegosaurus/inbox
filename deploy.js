5e8d993bfebbdd3d814692cf9e87a63fbd14f623
Justin Smith <smithjustin158@gmail.com>
2021-04-23 19:18:25 -0400

    update deploy

--- deploy.js
+++ deploy.js
@@ -16,7 +16,6 @@ const deploy = async () => {
     const result = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode, arguments: ['Hi there!']})
         .send({ gas: '1000000', from: accounts[0] });
-
     console.log('Contract deployed to', result.options.address);
 };
 deploy();
